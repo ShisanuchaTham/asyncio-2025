0f21ab84a669659a02ce1b1e64e83b03e0713bdc
ShisanuchaTham <6610301009@cdti.ac.th>
2025-06-11 11:01:34 +0700

    add the time to make burger for 1 burger

--- 1-intro/2-threading.py
+++ 1-intro/2-threading.py
@@ -3,6 +3,7 @@ import threading
 from datetime import datetime
 
 def make_burger(student_id):
+    start = time.time()
     print(f"[{datetime.now().strftime('%H:%M:%S')}] เริ่มทำเบอร์เกอร์ให้นักเรียนคนที่ {student_id}")
     
     print(f"[{datetime.now().strftime('%H:%M:%S')}] 1. ทอดเบอร์เกอร์...")
@@ -16,7 +17,8 @@ def make_burger(student_id):
 
     print(f"[{datetime.now().strftime('%H:%M:%S')}] 4. ห่อเบอร์เกอร์...")
     time.sleep(5)
-    print(f"[{datetime.now().strftime('%H:%M:%S')}] เสร็จแล้ว! เบอร์เกอร์ของนักเรียนคนที่ {student_id}")
+    end = time.time()
+    print(f"[{datetime.now().strftime('%H:%M:%S')}] เสร็จแล้ว! เบอร์เกอร์ของนักเรียนคนที่ {student_id} ใช้เวลา {end - start:.6f}")
 
 def main():
     start = time.time()
