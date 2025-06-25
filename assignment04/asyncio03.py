a0b3acb6f82315e1ed8c5cbb1c4fdd43c13e850e
ShisanuchaTham <6610301009@cdti.ac.th>
2025-06-25 11:05:16 +0700

    asy03,04

--- assignment04/asyncio03.py
+++ assignment04/asyncio03.py
@@ -1,2 +1,17 @@
 # Create 2 Tasks with High-Level API
+import asyncio
 
+async def download_image(name, delay):
+    print(f"{name} loading")
+    await asyncio.sleep(delay)
+    print(f"{name} loading comple")
+
+async def main():
+    # create 2 task in 1 time
+    task1 = asyncio.create_task(download_image("image1",2))
+    task2 = asyncio.create_task(download_image("image2",4))
+
+    await task1
+    await task2
+
+asyncio.run(main())
--- assignment04/asyncio04.py
+++ assignment04/asyncio04.py
@@ -1,3 +1,21 @@
 # example of starting many tasks and getting access to all tasks
+import time
+import asyncio
 
+# coroutine for task
+async def download_image(name, delay):
+    print(f"{time.ctime()} {name} loading")
+    await asyncio.sleep(delay)
+    print(f"{time.ctime()} {name} loading comple")
 
+async def main():
+    # report message
+    print(f'{time.ctime()} main coroutine started')
+    # start many task
+    started_tasks = [asyncio.create_task(download_image(i, i)) for i in range(3)]
+    # allow some of task time to start
+    await asyncio.sleep(0.1)
+    for task in started_tasks:
+        await task
+
+asyncio.run(main())
