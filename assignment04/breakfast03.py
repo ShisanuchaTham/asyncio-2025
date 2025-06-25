39174944f9765d64ee1f0453a25d23f749aa90dc
ShisanuchaTham <6610301009@cdti.ac.th>
2025-06-25 12:03:41 +0700

    breakfast03

--- assignment04/breakfast03.py
+++ assignment04/breakfast03.py
@@ -8,14 +8,14 @@ from time import sleep, time
 
 async def make_coffee():  # 1
     print("coffee: prepare ingridients")
-    await asyncio.sleep(1)
+    sleep(1)
     print("coffee: waiting...")
     await asyncio.sleep(5)  # 2: pause, another tasks can be run
     print("coffee: ready")
 
 async def fry_eggs():  # 1
     print("eggs: prepare ingridients")
-    await asyncio.sleep(1)
+    sleep(1)
     print("eggs: frying...")
     await asyncio.sleep(3)  # 2: pause, another tasks can be run
     print("eggs: ready")
