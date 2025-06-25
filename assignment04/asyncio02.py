4d42842f3648405528f0468d93880a7cb26acf9a
ShisanuchaTham <6610301009@cdti.ac.th>
2025-06-25 10:49:53 +0700

    asy02

--- assignment04/asyncio02.py
+++ assignment04/asyncio02.py
@@ -1,1 +1,13 @@
 # Create 1 Task with High-Level API
+import asyncio
+
+async def do_something():
+    print("start work...")
+    await asyncio.sleep(2)
+    print("compled work.")
+
+async def main():
+    task = asyncio.create_task(do_something())
+    await task
+    
+asyncio.run(main())
